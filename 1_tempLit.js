76e150e26cfa7564426705d7632eda29947bb41e
anak-ut <031317495@ecampus.ut.ac.id>
2024-07-18 23:21:25 +0700

    rename file ordered start from 1

---
+++ 1_tempLit.js
@@ -0,0 +1,11 @@
+// belajar template literals
+
+
+const word1 = 'jokon';
+const word2 = 'konsol';
+
+const fullname1 = word1 +' ' +word2; // cara lama
+console.log(fullname1);
+const fullname2 = `${word1} ${word2}`;
+console.log(fullname2); // template literals
+
