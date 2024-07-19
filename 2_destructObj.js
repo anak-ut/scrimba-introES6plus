750396464fbbcd4b9fe05072fb555578431267d8
anak-ut <031317495@ecampus.ut.ac.id>
2024-07-19 17:01:27 +0700

    challenge for destructing object

--- 2_destructObj.js
+++ 2_destructObj.js
@@ -16,5 +16,19 @@ console.log(player.addres.city);
 // mari kita destruct ges
 
 const { name,club,addres : {city} }= player;
-let biodata = `namanya adalah ${name} dari klub ${club} dari kota ${city}`;
+const biodata = `namanya adalah ${name} dari klub ${club} dari kota ${city}`;
 console.log(biodata);
+
+// challenge
+const student = {
+    nama : 'gibrotot',
+    age : 69,
+    projek : {
+        gameDadu : "ngocok dadu ampe lemess"
+    }
+};
+
+// mari kita destructure challenge ini
+const {nama, age, projek, projek: {gameDadu}} = student;
+let biodataStudent = `si ${nama} berumur ${age} punya game ${projek} yaitu ${gameDadu}`;
+console.log(biodataStudent);
