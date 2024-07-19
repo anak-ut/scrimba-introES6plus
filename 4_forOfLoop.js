3eca25cc677c800c409200693641f5f909f997e3
anak-ut <031317495@ecampus.ut.ac.id>
2024-07-19 20:39:28 +0700

    for loop challenge

---
+++ 4_forOfLoop.js
@@ -0,0 +1,19 @@
+let incomes = [30,55,12];
+let total = 0;
+for (let incom of incomes){
+    console.log(incom);
+    total += incom;
+}
+console.log(total);
+
+console.log('--------------------')
+//lets iterate string
+
+let fullnames = 'jokontolol';
+for (let char of fullnames){
+    console.log(char);
+}
+
+// challenge
+console.log('-----challenge-----')
+
