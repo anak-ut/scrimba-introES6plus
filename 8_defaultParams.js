0a4cb821485505b0daa85a6d4a9ab2e6e4924026
anak-ut <031317495@ecampus.ut.ac.id>
2024-07-21 00:18:37 +0700

    challenge accepted

--- 8_defaultParams.js
+++ 8_defaultParams.js
@@ -5,4 +5,11 @@ const anakBabi = (item='saya')=>{
 anakBabi('jokontol');
 anakBabi();
 
-// default params is passing value event the param is empty
+// default params is passing value event the argument/param is empty
+console.log('----- challenge -----');
+
+const buyShop = (food='something')=>{
+    return `i'm going to buy ${food}`;
+}
+console.log(buyShop('sempak goku'));
+console.log(buyShop());
