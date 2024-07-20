ddbcab3e00e5e76f7c8d676f904151ab5dbf5ee0
anak-ut <031317495@ecampus.ut.ac.id>
2024-07-20 22:55:36 +0700

    restOP

---
+++ 6_restOp.js
@@ -0,0 +1,11 @@
+function add(...nums){
+    console.log(arguments);// arguments adalah object bawaan fungsi
+    //untuk simpan semua argumen/ parameter dan menampilkan indeks 
+    // output : [Arguments]{'0':4, '1':5, '2':7, '3':8, '4':12}
+    // urutan indeks dan array
+    // agar nums bisa di-iterate, gunakan spread operator pada parameter
+    // spread op konvert (...) to [...] agar bs di-iterate
+    console.log(nums)
+}
+
+add(4,5,7,8,12)
