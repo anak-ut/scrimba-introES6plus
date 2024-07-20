50cc2f5168c2a25282e5ab5938c5726052fd1561
anak-ut <031317495@ecampus.ut.ac.id>
2024-07-20 22:57:10 +0700

    edit dikit ga ngaruh

--- 6_restOp.js
+++ 6_restOp.js
@@ -1,8 +1,10 @@
 function add(...nums){
-    console.log(arguments);// arguments adalah object bawaan fungsi
+    //console.log(arguments);
+    // 'arguments' adalah object bawaan fungsi
     //untuk simpan semua argumen/ parameter dan menampilkan indeks 
     // output : [Arguments]{'0':4, '1':5, '2':7, '3':8, '4':12}
     // urutan indeks dan array
+    
     // agar nums bisa di-iterate, gunakan spread operator pada parameter
     // spread op konvert (...) to [...] agar bs di-iterate
     console.log(nums)
