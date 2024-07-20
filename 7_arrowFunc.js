c4f33534899cf52ac2f377f82493d4459254cde6
anak-ut <031317495@ecampus.ut.ac.id>
2024-07-20 23:00:33 +0700

    section arrow func

---
+++ 7_arrowFunc.js
@@ -0,0 +1,11 @@
+// contoh funct declaration
+// fungsi biasa
+function menuMakan(){
+    return 'bayar dulu baru mesen !'
+};
+
+// contoh funct anonymous
+// tidak ada nama function, langsung dimasukkan ke variabel
+const utangDulu = function(){
+    return 'bayar utang lah wak ..'
+}
