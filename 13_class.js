fa06095d5a9ccd39dd3d58504507e16500579b31
anak-ut <031317495@ecampus.ut.ac.id>
2024-07-21 19:30:02 +0700

    mengambil catWomen

--- 13_class.js
+++ 13_class.js
@@ -1,10 +1,24 @@
 export class Animal{
-    constructor(type,legs){
+    constructor(type,legs){ // cara initialize  property class Animal
         this.type = type;
         this.legs = legs;
     }
-    // define func
-    makeSound(sound='loud noise'){
+    // define func dalam hal ini adalah method
+    makeSound(sound='loud noise'){ // initialize method
+        console.log(sound);
+    }
+    static cetak10(){ //static method
+        console.log('cetak 10')
+    }
+    static creator = 'god'; // static property
+}
+
+export class Cat extends Animal { // membuat class Cat turunan dari Animal
+    constructor(type,legs,tail){
+        super(type,legs);
+        this.tail = tail;
+    }
+    makeSound(sound='miaww'){
         console.log(sound);
     }
 }
--- 13a_class.js
+++ 13a_class.js
@@ -1,4 +1,7 @@
-import { Animal } from "./13_class.js";
+import { Animal,Cat } from "./13_class.js";
+
+// method dan properti class Animal tidak dapat dipanggil langsung,
+// harus lewat membuat objek baru
 let cat = new Animal('cat',4);
 console.log(cat);
 
@@ -7,3 +10,12 @@ cat.legs=99;
 console.log(cat);
 console.log(`${cat.type} has ${cat.legs} legs`);
 cat.makeSound('ngeoooong');
+
+// method static dapat dipanggil tanpa perlu membuat objek baru
+Animal.cetak10();
+console.log(Animal.creator);
+
+// panggil class Cat
+let catWomen = new Cat('catwomen',2);
+console.log(catWomen.legs);
+catWomen.makeSound('saya adalah cat women');
