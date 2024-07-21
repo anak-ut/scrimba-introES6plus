dc219cd55ba05004610d3069360fa912ce4c5dff
anak-ut <031317495@ecampus.ut.ac.id>
2024-07-21 12:40:16 +0700

    anomali const

---
+++ 10_letconst.js
@@ -0,0 +1,33 @@
+if (false){
+    var example = 1;
+    let example1 = 2;
+    const example2 = 3
+}
+console.log(example); // output : undefined
+// variabel ada tapi belum bernilai, padahal ada loh =1 ? wkwk
+// sepertinya power var ini agak OP, karena walaupun di dalam block,
+// masih terdeteksi keberadaannya, beda dengan let const.
+// ini disebut hoisting
+
+// console.log(example1); // output : isnot defined
+// variabel belum dibuat, alias tidak ada, padahal ada ya di dalam block
+// const tidak bisa reassign other value/type data
+// tapi dalam bentuk array/ object, bisa ditambah kurang isinya.
+
+const konstanta = [];
+konstanta.push('bisa')
+console.log(konstanta)
+
+const konStanta = {};
+konStanta.nama = 'jokontold';
+console.log(konStanta);
+
+const cobaReassign = 12;
+console.log(cobaReassign);
+cobaReassign = 13;
+// console.log(cobaReassign) //gabisa reassign karna typeError
+
+// console.log(example2); // output: isnot defined
+// nasib sama dengan let
+
+// kesimpulan : let const kalau didalam block, no power jika diakses dari luar
