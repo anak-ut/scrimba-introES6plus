63faae37bd091d1a065cea5f8b8288b34d0531e5
anak-ut <031317495@ecampus.ut.ac.id>
2024-07-21 18:14:34 +0700

    playing class - OOP javascript

--- 13a_class.js
+++ 13a_class.js
@@ -1,4 +1,4 @@
 import { Animal } from "./13_class.js";
 let cat = new Animal('cat',4);
 console.log(cat);
-console.log(`${cat.type} has ${cat.legs} legs`)
+console.log(`${cat.type} has ${cat.legs} legs`);
