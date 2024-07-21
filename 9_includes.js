cdcc3024c430cfa612136c61e71de4bcf76ba7ad
anak-ut <031317495@ecampus.ut.ac.id>
2024-07-21 12:12:52 +0700

    perbedaan includes() dan indexOf()

---
+++ 9_includes.js
@@ -0,0 +1,9 @@
+// includes() method untuk mengecek apakah ada elemen tertentu di dalam array
+// includes() return boolean true/false
+
+// sedangkan indexOf() me-return posisi index yag dicari
+// dan return  -1 jika tidak ditemukan
+
+let nuArray = [1,2,3,4,5];
+console.log(nuArray.indexOf(4)); // return 3 (posisi ke-3) elemen 4
+console.log(nuArray.includes(4)); //return true, ada elemen 4 di array tsb
