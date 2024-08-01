23ab2aa4e5adb45f6b29e6142e087212b799873a
anak-ut <031317495@ecampus.ut.ac.id>
2024-08-01 13:58:06 +0700

    belajar sync await

--- 16_asyncAwait.js
+++ 16_asyncAwait.js
@@ -1,11 +1,36 @@
 let potos = [];
-function fotoUp(){
-    let uploadStat = new Promise((resolve, reject)=>{
-        potos.up = 'gambar gw';
-        resolve(potos)
-    },3000)
+
+// function fotoUp(){
+//     let uploadStat = new Promise((resolve, reject)=> {
+//         setTimeout(()=>{
+//             potos.push(' gambar xx');
+//             resolve(' foto terupload')
+//         },4000)
+//     })
+//     let result  = uploadStat
+//     console.log(result);
+//     console.log(potos.length);
+//     console.log(potos)
+
+// }
+// output : result = Pending, length =0; potos = kosong
+// karna func fotoUp tetap menjalankan kode ke bawah walopun ada fungsi uploadStat
+// yang masih berjalan 4000ms.
+// makanya fungsi didalam uploadStat tidak terjalankan.karna JS melewati dan
+// menjalankan fungsi2 dibawahnya
+
+async function fotoUp(){
+    let uploadStat = new Promise((resolve, reject)=> {
+        setTimeout(()=>{
+            potos.push(' gambar xx');
+            resolve(' foto terupload')
+        },4000)
+    })
+    let result  = await uploadStat
+    console.log(result);
+    console.log(potos.length);
+    console.log(potos)
+
 }
 
-let hasil = rezult
-console.log(poto Album)
 fotoUp();
