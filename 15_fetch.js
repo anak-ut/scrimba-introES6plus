7ad3dfc6e89e09d159b327a04d86010f9de5f2e5
anak-ut <031317495@ecampus.ut.ac.id>
2024-08-01 13:31:54 +0700

    fetch data pake async function

--- 15_fetch.js
+++ 15_fetch.js
@@ -1,21 +1,37 @@
 // restful api - https://jsonplaceholder.typicode.com/
 
-fetch('https://jsonplaceholder.typicode.com/posts/102')
-.then((response)=>response.json())
-.then((json)=>console.log(json));
+// fetch('https://jsonplaceholder.typicode.com/posts/102')
+// .then((response)=>response.json())
+// .then((json)=>console.log(json));
+
+const link = 'https://jsonplaceholder.typicode.com/todos/1' ;
+async function tarikData(){
+    try {
+        const response = await fetch(link);
+        if (!response.ok){
+            throw new Error(`response status : ${response.status}`);
+        }
+        const json = await response.json();
+        console.log(json);
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+tarikData();
 
 // make post
 
-fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: 'POST',
-    body: JSON.stringify({
-        title: 'juancokimak jokowee',
-        body: 'juancok asu tenan jokontot luhut anyenk makan 7500',
-        userId: 6969,
-    }),
-    headers: {
-        'Content-type': 'application/json; charset=UTF-8'
-    },
-})
-.then((response)=>response.json())
-.then((json)=>console.log(json));
+// fetch('https://jsonplaceholder.typicode.com/posts', {
+//     method: 'POST',
+//     body: JSON.stringify({
+//         title: 'juancokimak jokowee',
+//         body: 'juancok asu tenan jokontot luhut anyenk makan 7500',
+//         userId: 6969,
+//     }),
+//     headers: {
+//         'Content-type': 'application/json; charset=UTF-8'
+//     },
+// })
+// .then((response)=>response.json())
+// .then((json)=>console.log(json));
---
+++ 16_asyncAwait.js
@@ -0,0 +1,11 @@
+let potos = [];
+function fotoUp(){
+    let uploadStat = new Promise((resolve, reject)=>{
+        potos.up = 'gambar gw';
+        resolve(potos)
+    },3000)
+}
+
+let hasil = rezult
+console.log(poto Album)
+fotoUp();
