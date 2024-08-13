c4767c6c1549a83d8c93fb35f8aebb30fa11c817
muhammad ali <031317495@ecampus.ut.ac.id>
2024-08-13 19:05:28 +0700

    membuat const mu sekeras batu tidak bisa ditambah/ kurang

--- 10_letconst.js
+++ 10_letconst.js
@@ -33,3 +33,13 @@ console.log(cobaReassign);
 // nasib sama dengan let
 
 // kesimpulan : let const kalau didalam block, no power jika diakses dari luar
+
+// meskipun const diharapkan tidak dapat direassign, tetapi tidak jamin 100%
+// const tidak diubah
+// untuk mencegah ada yang ubah const, lakukan dengan property Object.freeze()
+const rockVariable = Object.freeze([1,3,3,99]);
+// rockVariable.push(69); // dicomment agar tidak error. array method push gagal ubah rockVariable
+console.log(rockVariable);
+// output : TypeError: Cannot add property 4, object is not extensible
+
+// sumber bacaan : https://www.joshwcomeau.com/javascript/the-const-deception/
